911ad4b79084351e7a62ef1ca63690e265b1d9a2
matvejevs <matvejevs@MATVEJEVS-W>
2021-05-27 11:03:17 +0300

    variables

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-print(f"20 days are {20 * 24 * 60} minutes")
-print(f"35 days are {35 * 24 * 60} minutes")
+calculation_to_seconds = 24 * 60 * 60
+
+print(f"20 days are {20 * calculation_to_seconds} seconds")
+
