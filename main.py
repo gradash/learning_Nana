b9d92bc6237e24957ccc3dbfc6389a479b78fbab
matvejevs <matvejevs@MATVEJEVS-W>
2021-05-27 14:37:37 +0300

    functions

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 calculation_to_seconds = 24 * 60 * 60
+name_of_unit = "seconds"
 
-print(f"20 days are {20 * calculation_to_seconds} seconds")
+
+def days_to_units():
+    print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
+    print("All Good!")
+
+
+days_to_units()
 
