72f7f61b149ffc72ca57115bcce6275698a3741f
matvejevs <matvejevs@MATVEJEVS-W>
2021-05-27 15:13:06 +0300

    input and transform variable types

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ calculation_to_seconds = 24 * 60 * 60
 name_of_unit = "seconds"
 
 
-def days_to_seconds(number_of_days, custom_message):
-    print(f"{number_of_days} days are {number_of_days * calculation_to_seconds} {name_of_unit}")
-    print(custom_message)
+def days_to_seconds(number_of_days):
+    return f"{number_of_days} days are {number_of_days * calculation_to_seconds} {name_of_unit}"
 
 
-def scope_check(number_of_days):
-    my_var = "variable defined inside function"
-    print(name_of_unit)
-    print(number_of_days)
-    print(my_var)
+user_input = input("Enter number of days: ")
+user_input_cast_to_int = int(user_input)
 
-
-days_to_seconds(10, "All Good!")
-days_to_seconds(30, "It' Works!")
-
-scope_check(22)
-
-
+calculated_value = days_to_seconds(user_input_cast_to_int)
+print(calculated_value)
